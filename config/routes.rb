be8b5fb7e6de76834422f749e3406a38f9e52b02
Isabel Dockemeyer <isabel.dockemeyer@web.de>
2023-08-17 16:25:44 +0200

    Updated Features and Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists, only: [:index, :create, :show]
+  # do resources :bookmark, only: [:new, :create]
+  # end
+  # do resources :bookmark, only: [:delete]
 end
